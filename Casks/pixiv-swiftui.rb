6c8d5c139905dca07b5df7981679915c938e91c0
Eslzzyl <eslzzyl@163.com>
2026-03-16 16:29:15 +0800

    init commit

---
+++ Casks/pixiv-swiftui.rb
@@ -0,0 +1,19 @@
+cask "pixiv-swiftui" do
+  version "0.12.1"
+
+  if Hardware::CPU.arm?
+    sha256 "8c6c65ffb2417e4c38ba3075e6007ec7f9da29953a3006a0ce9d54d522b88b2e"
+    url "https://github.com/Eslzzyl/Pixiv-SwiftUI/releases/download/v#{version}/Pixiv-SwiftUI-arm64.dmg"
+  elsif Hardware::CPU.intel?
+    sha256 "7fc14da559746a2d8ba08277cf2aa2b8f00239e9e5b2ac0e21518cf0af6b8c0b"
+    url "https://github.com/Eslzzyl/Pixiv-SwiftUI/releases/download/v#{version}/Pixiv-SwiftUI-x86_64.dmg"
+  end
+
+  name "Pixiv-SwiftUI"
+  desc "A SwiftUI-based Pixiv third-party client"
+  homepage "https://github.com/Eslzzyl/Pixiv-SwiftUI"
+
+  app "Pixiv-SwiftUI.app"
+
+  zap trash: "~/Library/Application Support/com.pixiv.swiftui"
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# homebrew-tap
+
+Unofficial Homebrew tap for Pixiv-SwiftUI.
+
+## Usage
+
+```bash
+brew tap eslzzyl/tap
+brew install --cask pixiv-swiftui
+```
+
+## Uninstall
+
+```bash
+brew uninstall --cask pixiv-swiftui
+brew untap eslzzyl/tap
+```
